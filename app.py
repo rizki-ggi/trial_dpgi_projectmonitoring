fda58775d98e6b22a5efd4bfeecad9eac03d72a2
rizki-ggi <165998304+rizki-ggi@users.noreply.github.com>
2024-12-13 17:04:37 +0700

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ if not check_password():
 # Display the dashboard
 st.subheader("Dashboard from Looker Studio")
 looker_studio_url = "https://lookerstudio.google.com/embed/reporting/7b165fb5-3c3e-4429-b162-a4dbe6438e28/page/xFVvD"
-components.iframe(looker_studio_url, width=800, height=600)
+components.iframe(looker_studio_url, width=2000, height=1600)
 
 # Add Sign Out button below the dashboard
 st.write("")  # Add a spacer
